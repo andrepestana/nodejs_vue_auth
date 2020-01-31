83e194433791970e2628b6a0e279206cef1d2932
andrepestana <andrepestana@gmail.com>
2020-01-31 08:07:52 -0800

    created TODO; added condition when using fake database

---
+++ TODO.md
@@ -0,0 +1,6 @@
+#TODO
+
+- save encrypted passwords
+- fix signup validation
+- create posts component
+- create user signup validation based on configuration, eg. minPasswordLength=6
--- src/server/server.js
+++ src/server/server.js
@@ -21,20 +21,42 @@ if (process.env.ALLOW_ACCESS_FROM_ANY_ORIGIN) {
   });
 }
 
-const posts = [
-  {
-    username: 'a@a.com',
-    title: 'Post 1'
-  },
-  {
-    username: 'test@test.com',
-    title: 'Post 2'
-  }
-]
+let posts = []
 
-app.get('/posts', authenticateToken, (req, res) => {
-  res.json(posts.filter(post => post.username === req.user.name))
-})
+if (process.env.FAKE_PERSISTENT_DATA) {
+  posts = [
+    {
+      username: 'a@a.com',
+      title: 'Post 1',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit lectus id arcu lobortis, eu congue lorem euismod. Nunc aliquam ullamcorper interdum. Fusce diam nibh, accumsan sed sollicitudin a, accumsan sed metus. Donec vestibulum congue lacinia. Mauris odio elit, auctor sed ligula eget, tempor convallis tortor. Nam consectetur ex in elementum auctor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris sodales molestie magna, at vestibulum magna ornare at. Aenean eget ex eros. Pellentesque vel libero id purus semper ullamcorper. Cras eget venenatis est. Quisque venenatis eu est eget mollis. Integer tincidunt at quam eu scelerisque.'
+    },
+    {
+      username: 'a@a.com',
+      title: 'Post 2',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit lectus id arcu lobortis, eu congue lorem euismod. Nunc aliquam ullamcorper interdum. Fusce diam nibh, accumsan sed sollicitudin a, accumsan sed metus. Donec vestibulum congue lacinia. Mauris odio elit, auctor sed ligula eget, tempor convallis tortor. Nam consectetur ex in elementum auctor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris sodales molestie magna, at vestibulum magna ornare at. Aenean eget ex eros. Pellentesque vel libero id purus semper ullamcorper. Cras eget venenatis est. Quisque venenatis eu est eget mollis. Integer tincidunt at quam eu scelerisque.'
+    },
+    {
+      username: 'test@test.com',
+      title: 'Post 3',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit lectus id arcu lobortis, eu congue lorem euismod. Nunc aliquam ullamcorper interdum. Fusce diam nibh, accumsan sed sollicitudin a, accumsan sed metus. Donec vestibulum congue lacinia. Mauris odio elit, auctor sed ligula eget, tempor convallis tortor. Nam consectetur ex in elementum auctor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris sodales molestie magna, at vestibulum magna ornare at. Aenean eget ex eros. Pellentesque vel libero id purus semper ullamcorper. Cras eget venenatis est. Quisque venenatis eu est eget mollis. Integer tincidunt at quam eu scelerisque.'
+    },
+    {
+      username: 'test@test.com',
+      title: 'Post 4',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc hendrerit lectus id arcu lobortis, eu congue lorem euismod. Nunc aliquam ullamcorper interdum. Fusce diam nibh, accumsan sed sollicitudin a, accumsan sed metus. Donec vestibulum congue lacinia. Mauris odio elit, auctor sed ligula eget, tempor convallis tortor. Nam consectetur ex in elementum auctor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris sodales molestie magna, at vestibulum magna ornare at. Aenean eget ex eros. Pellentesque vel libero id purus semper ullamcorper. Cras eget venenatis est. Quisque venenatis eu est eget mollis. Integer tincidunt at quam eu scelerisque.'
+    }
+  ]
+} else {
+  throw 'Not implemented yet for non fake persistent data'
+}
+
+if (process.env.FAKE_PERSISTENT_DATA) {
+  app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+  })
+} else {
+  throw 'Not implemented yet for non fake persistent data'
+}
 
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
