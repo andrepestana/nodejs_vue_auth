1f01ca9f22691c006b9c30d54b7cd5173d6f8a3b
andrepestana <andrepestana@gmail.com>
2020-02-14 20:24:22 -0800

    Fixed user ssesion layout

--- src/web/components/auth/userSessions.vue
+++ src/web/components/auth/userSessions.vue
@@ -2,25 +2,40 @@
     <div class="user-session-box">
         <h1 class="text-center">User sessions</h1>
         <messages></messages>
-        <div class="container border border-dark">
-            <div class="row bg-dark text-white">
-                <div class="col-2 border border-dark p-2 text-wrap">Session started</div>
-                <div class="col-2 border border-dark p-2 text-wrap">Session expiration time</div>
-                <div class="col-4 border border-dark p-2 text-wrap">User Agent</div>
-                <div class="col-2 border border-dark p-2 text-wrap">Remote Address</div>
-                <div class="col-1 border border-dark p-2 text-wrap">Revoked</div>
-                <div class="col-1 border border-dark p-2 text-wrap">Action</div>
-            </div>
-            <div class="row" 
+        <div>
+            <div 
                 v-for="(userSession,index) in userSessions" 
                 :key="index"
-                v-bind:class="{ 'alert-warning': isCurrentSession(index) }">
-                <div class="col-2 border border-dark p-2 text-wrap">{{ new Date(userSession.refreshTokenCreatedDate) | formatDateTime }}</div>
-                <div class="col-2 border border-dark p-2 text-wrap">{{ new Date(userSession.refreshTokenExpirationDate) | formatDateTime }}</div>
-                <div class="col-4 border border-dark p-2 text-wrap">{{ userSession.clientInfo.userAgent }}</div>
-                <div class="col-2 border border-dark p-2 text-wrap">{{ userSession.remoteAddress }}</div>
-                <div class="col-1 border border-dark p-2 text-wrap">{{ userSession.revoked | yesOrNo }}</div>
-                <div class="col-1 border border-dark p-2 text-wrap"><a href="#" v-if="!userSession.revoked && !isCurrentSession(index)" @click="logItOut(index)">Log it out</a></div>
+                v-bind:class="{ 'alert-warning': isCurrentSession(index) }"
+                class="user-sesion-item">
+              <div>
+                <div class="font-weight-bold d-md-table-cell label">Started at: </div>
+                <div class="d-md-table-cell">{{ new Date(userSession.refreshTokenCreatedDate) | formatDateTime }}</div>
+              </div>
+              <div>  
+                <div class="font-weight-bold d-md-table-cell label">Expires at:</div>
+                <div class="d-md-table-cell">{{ new Date(userSession.refreshTokenExpirationDate) | formatDateTime }}</div>
+              </div>
+              <div>
+                <div class="font-weight-bold d-md-table-cell label">User Agent:</div>
+                <div class="d-md-table-cell">{{ userSession.clientInfo.userAgent }}</div>
+              </div>
+              <div>  
+                <div class="font-weight-bold d-md-table-cell label">Remote Address:</div>
+                <div class="d-md-table-cell">{{ userSession.remoteAddress }}</div>
+              </div>
+              <div>  
+                <div class="font-weight-bold d-md-table-cell label">Revoked:</div>
+                <div class="d-md-table-cell">{{ userSession.revoked | yesOrNo }}</div>
+              </div>
+              <div>  
+                <div class="">
+                  <button v-if="!userSession.revoked && !isCurrentSession(index)" 
+                          @click="logItOut(index)"
+                          class="btn btn-dark">
+                    Log it out
+                  </button></div>
+              </div>
             </div>
         </div>
     </div>
@@ -55,5 +70,15 @@
     border: 1px solid #eee;
     padding: 20px;
     box-shadow: 0 2px 3px #ccc;
+  }
+  .user-sesion-item {
+    max-width: 1000px;
+    margin: 20px auto;
+    border: 1px solid #eee;
+    padding: 20px;
+    box-shadow: 0 2px 3px #ccc;
+  }
+  .label {
+    width : 150px;
   }
 </style>
