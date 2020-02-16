ec118e3739aa950169d9a5ccc849a23c8bd6aa00
andrepestana <andrepestana@gmail.com>
2020-02-15 21:50:13 -0800

    Added user.id to user

--- src/server/dao/userDao.js
+++ src/server/dao/userDao.js
@@ -4,9 +4,10 @@ const userDao = {
     saveUser(user) {
         if (process.env.FAKE_PERSISTENT_DATA) {
             let foundIndex = users.findIndex(u => u.username == user.username);
-            if(foundIndex === -1)
+            if(foundIndex === -1) {
+                user.id = new Date().getTime()
                 users.push(user)
-            else {
+            } else {
                 users[foundIndex] = user;
             }
         } else {
@@ -26,6 +27,13 @@ const userDao = {
     retrieveUserByUsername(username) {
         if (process.env.FAKE_PERSISTENT_DATA) {
             return users.find(u => u.username === username)
+        } else {
+            throw 'Not implemented yet for non fake persistent data'
+        }
+    },
+    retrieveUserById(id) {
+        if (process.env.FAKE_PERSISTENT_DATA) {
+            return users.find(u => u.id === id)
         } else {
             throw 'Not implemented yet for non fake persistent data'
         }
